08878cb7ac0333de61ef96c29f817b7d4de2764c
sondev <sondev811@gmail.com>
2021-12-30 01:20:04 +0700

    update url

--- src/services/nimo.service.js
+++ src/services/nimo.service.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 class Nimo {
     async get(endpoint) {
         try {
-            // let url = `https://nimo-vote-be.vercel.app/api/${endpoint}`;
-            let url = `http://localhost:5000/api/${endpoint}`;
+            let url = `https://nimo-vote-be.vercel.app/api/${endpoint}`;
             const response = await axios.get(url);
             return response.data;
         } catch (error) {
